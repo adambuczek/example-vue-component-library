e26590c449b589bca65c8ab69b4bde5b01beb89d
Adam Buczek <adambuczek@gmail.com>
2020-11-06 17:24:58 +0100

    backport to vue2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <PageTitle
-    text="Page title looks like this"
-  />
-  <CaptionedImage
-    url="https://picsum.photos/720/300"
-    alt="This is alt and title"
-    caption="This is a random image"
-  />
-  <ThreeColumnText
-    text="Quaerat suscipit aut debitis sequi voluptas eum tenetur impedit. Est ducimus eos autem iste amet. Veritatis aperiam qui eveniet quam at est. Esse aliquid officiis incidunt reiciendis inventore voluptatibus ullam consequatur. Mollitia aut aut atque dicta quia. Dolores ut fugiat omnis maiores itaque omnis sed. Vel ut reiciendis ea distinctio quis occaecati. Ut quibusdam tempora nesciunt facere ab explicabo. Sed non enim expedita. Aut vel voluptas ad neque quae ex nulla rem. Sit omnis eius odio saepe."
-  />
+  <div>
+    <PageTitle
+      text="Page title looks like this"
+    />
+    <CaptionedImage
+      url="https://picsum.photos/720/300"
+      alt="This is alt and title"
+      caption="This is a random image"
+    />
+    <ThreeColumnText
+      text="Quaerat suscipit aut debitis sequi voluptas eum tenetur impedit. Est ducimus eos autem iste amet. Veritatis aperiam qui eveniet quam at est. Esse aliquid officiis incidunt reiciendis inventore voluptatibus ullam consequatur. Mollitia aut aut atque dicta quia. Dolores ut fugiat omnis maiores itaque omnis sed. Vel ut reiciendis ea distinctio quis occaecati. Ut quibusdam tempora nesciunt facere ab explicabo. Sed non enim expedita. Aut vel voluptas ad neque quae ex nulla rem. Sit omnis eius odio saepe."
+    />
+  </div>
 </template>
 
 <script>
